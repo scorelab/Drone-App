15124a65fee638674222dfdab785eeb9ddee7cd0
Lasith-Niro <lasithniro.online@gmail.com>
2017-01-04 17:48:11 +0530

    add firebase realtime database

---
+++ app/src/main/java/com/dji/GSDemo/GoogleMap/showMe.java
@@ -0,0 +1,22 @@
+package com.dji.GSDemo.GoogleMap;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+/**
+ * Created by lasithniro on 1/4/17.
+ */
+
+public class showMe  extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_firebase);
+    }
+}
